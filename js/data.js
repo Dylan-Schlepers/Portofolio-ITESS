2a808a910310f249280d4cf09b79c2c48cd7ffc0
Dylan Schlepers <dhschlepers@gmail.com>
2026-09-17 16:47:29 +0200

    Fix formatting of lesson plan description

--- js/data.js
+++ js/data.js
@@ -38,7 +38,7 @@ const ASSESSMENTS = [
   period: "Semester 1, Period 1",
   status: "planned",
   summary: "For this formative assesment we have to prepare our best lessen plan. The goal is to learn and evaluate big ideas about lesson planning, identify the steps, and define and create solid lesson objectives.",
-  description: "The concept for this lesson plan is: Migration, and is for a lesson length of 45 minutes. The targeted audience is 12-13 years old on a MYP school with a class of 24 students."
+  description: "The concept for this lesson plan is: Migration, and is for a lesson length of 45 minutes. The targeted audience is 12-13 years old on a MYP school with a class of 24 students.",
   image: "images/assessments/MyBestLessonPlan_ES.png". 
   },
 ];
